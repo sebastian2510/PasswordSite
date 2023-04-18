ff87e251a6628215d490c8c2b611dcbb813d9013
Sebastian-tec <seba9188@elev.tec.dk>
2023-04-18 09:25:10 +0200

    Init

---
+++ PasswordSite/Data/Joke.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace PasswordSite.Data
+{
+    public class Joke
+    {
+        static public string GetJoke()
+        {
+            RestClient client = new RestClient("https://icanhazdadjoke.com/");
+            RestRequest request = new RestRequest();
+            request.AddHeader("Accept", "application/json");
+
+            RestResponse response = client.Execute(request);
+            dynamic content = JObject.Parse(response.Content);
+            return content.joke;
+        }
+    }
+}
---
+++ PasswordSite/Data/Password.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using Newtonsoft.Json.Linq;
+
+
+namespace PasswordSite.Data
+{
+    public class Password
+    {
+        static public string GetPassword(string url)
+        {
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest();
+            RestResponse response = client.Execute(request);
+            dynamic content = JObject.Parse(response.Content);
+
+            return content.data;
+        }
+    }
+}
