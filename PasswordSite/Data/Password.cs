0c1d61a1024659907014222f7b01608464e4925a
Sebastian-tec <seba9188@elev.tec.dk>
2023-08-10 08:10:20 +0200

    Better usage of the api

--- PasswordSite/Data/Password.cs
+++ PasswordSite/Data/Password.cs
@@ -6,13 +6,18 @@ namespace PasswordSite.Data
 {
     public class Password
     {
-        static public string? GetPassword(string url)
+        public static string GetPassword(bool numbers, bool chars, bool caps, int length)
         {
-            RestClient client = new(url);
+            RestClient client = new("https://passwordinator.onrender.com/");
             RestRequest request = new();
-            return JsonConvert.DeserializeObject<PasswordAPI>(client.Execute(request).Content).Data;   
+            request.AddHeader("num", numbers);
+            request.AddHeader("char", chars);
+            request.AddHeader("caps", caps);
+            request.AddHeader("len", length);
+            RestResponse test = client.Execute(request);
+            string content = test.Content;
+            return JsonConvert.DeserializeObject<PasswordAPI>(content).Data; // The JsonConvert could/should be a different function, but whatever
         }
-
     }
     public class PasswordAPI
     {
